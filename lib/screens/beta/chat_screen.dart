cdc78611579e05a5c3679d81961967eed76696f2
NV-Phong <0h9ng03@gmail.com>
2025-01-07 21:15:13 +0700

    FEAT - update chat screen

--- lib/screens/beta/chat_screen.dart
+++ lib/screens/beta/chat_screen.dart
@@ -51,10 +51,10 @@ class _ChatScreenState extends State<ChatScreen> {
           if (data != null && data is Map<String, dynamic>) {
             chatHistory.add({
               'senderId': data['senderId'] ?? '',
+              'IDReceiver': data['IDReceiver'] ?? '',
               'message': data['content'] ?? '',
               'createdAt': data['createdAt'] ?? 'Unknown',
             });
-            // Cuộn xuống cuối
             scrollToBottom();
           }
         });
@@ -68,13 +68,18 @@ class _ChatScreenState extends State<ChatScreen> {
           if (data != null && data is List) {
             chatHistory = List<Map<String, String>>.from(data.map((message) {
               if (message is Map<String, dynamic>) {
+                bool isCurrentUser = message['IDReceiver'] == widget.userId;
                 return {
                   'message': message['Message']?.toString() ?? '',
                   'senderId': message['IDSender']?.toString() ?? '',
+                  'IDReceiver': message['IDReceiver']?.toString() ?? '',
                   'createdAt': message['CreateAt'] != null
                       ? DateFormat('yyyy-MM-dd HH:mm').format(
                           DateTime.parse(message['CreateAt'].toString()))
                       : 'Unknown',
+                  'isCurrentUser': isCurrentUser
+                      ? 'true'
+                      : 'false', // Đánh dấu tin nhắn của người nhận
                 };
               } else {
                 return {'message': '', 'senderId': '', 'createdAt': 'Unknown'};
@@ -115,12 +120,12 @@ class _ChatScreenState extends State<ChatScreen> {
       setState(() {
         chatHistory.add({
           'senderId': clientId!,
+          'IDReceiver': widget.userId,
           'message': text,
           'createdAt': DateFormat('yyyy-MM-dd HH:mm').format(DateTime.now()),
         });
         messageController.clear();
       });
-      // Cuộn xuống cuối
       scrollToBottom();
     }
   }
@@ -156,70 +161,109 @@ class _ChatScreenState extends State<ChatScreen> {
         centerTitle: true,
         backgroundColor: Colors.purple,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: chatHistory.length,
+              itemBuilder: (context, index) {
+                final chat = chatHistory[index];
+                bool isCurrentUser = chat['IDReceiver'] ==
+                    widget
+                        .userId; // Kiểm tra xem tin nhắn của người nhận hiện tại hay không
+                return _buildMessageBubble(
+                  chat['message'] ?? '',
+                  chat['createdAt'] ?? '',
+                  isCurrentUser, // Cập nhật lại logic căn lề
+                );
+              },
+            ),
+          ),
+          _buildMessageInput(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageBubble(
+      String message, String timestamp, bool isCurrentUser) {
+    return Align(
+      alignment: isCurrentUser ? Alignment.centerLeft : Alignment.centerRight,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+        decoration: BoxDecoration(
+          color: isCurrentUser ? Colors.blue.shade100 : Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(20),
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: ListView.builder(
-                controller: scrollController,
-                itemCount: chatHistory.length,
-                itemBuilder: (context, index) {
-                  final chat = chatHistory[index];
-                  bool isSentByMe = chat['senderId'] == clientId;
-                  return Align(
-                    alignment: isSentByMe
-                        ? Alignment.centerRight
-                        : Alignment.centerLeft,
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(vertical: 5),
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: isSentByMe ? Colors.blue[100] : Colors.grey[300],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(chat['message'] ?? ''),
-                          const SizedBox(height: 5),
-                          Text(
-                            'Sent at: ${chat['createdAt']}',
-                            style: const TextStyle(
-                                fontSize: 12, color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
+            Text(
+              isCurrentUser ? 'Other' : 'You',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color:
+                    isCurrentUser ? Colors.blue.shade700 : Colors.grey.shade700,
               ),
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: messageController,
-                    decoration: const InputDecoration(
-                      labelText: 'Enter your message',
-                      border: OutlineInputBorder(),
-                    ),
-                    onFieldSubmitted: (value) {
-                      if (value.trim().isNotEmpty) {
-                        sendMessage();
-                      }
-                    },
-                  ),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: sendMessage,
-                  child: const Text('Send'),
-                ),
-              ],
+            const SizedBox(height: 4),
+            Text(
+              message,
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              DateFormat('HH:mm dd/MM/yyyy').format(DateTime.parse(timestamp)),
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey.shade600,
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildMessageInput() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      color: Colors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: messageController,
+              decoration: InputDecoration(
+                hintText: 'Enter message...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          FloatingActionButton(
+            onPressed: () {
+              if (messageController.text.isNotEmpty) {
+                sendMessage();
+                messageController.clear();
+              }
+            },
+            child: const Icon(Icons.send),
+            mini: true,
+            backgroundColor: Colors.purple,
+          ),
+        ],
       ),
     );
   }
